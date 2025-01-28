05bf30ededd3818d176bf7daa22a41f35dbe7725
AnkitvishwakarmaML <ankitvishwakarma01122001@gmail.com>
2025-01-28 14:55:34 +0530

    model evaluation component added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,6 +1,4 @@
-from src.entity.config_entity import TrainingPipelineConfig ,DataIngestionConfig
 from src.exception  import MyException
-from src.entity.artifact_entity import DataIngestionArtifact
 from src.logger import logging
 import sys , os 
 from src.components.data_ingestion import DataIngestion
@@ -8,11 +6,17 @@ from src.components.data_ingestion import DataIngestion
 
 
 from src.components.data_validation import DataValidation
-from src.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig, DataTransformationConfig, ModelTrainerConfig
-from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ClassificationMetricArtifact, ModelTrainerArtifact
-from src.components.data_transformation import DataTransformation
+from src.entity.config_entity import (TrainingPipelineConfig, DataIngestionConfig, 
+                                      DataValidationConfig, DataTransformationConfig,
+                                      DataTransformationConfig, ModelTrainerConfig,
+                                      ModelEvaluationConfig)
+from src.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact, 
+                                        DataTransformationArtifact, ClassificationMetricArtifact,
+                                        ModelTrainerArtifact, ModelEvaluationArtifact)
+from src.components.data_transformation import DataTransformation, DataValidationArtifact
 from src.components.model_trainer import ModelTrainer 
-
+from src.components.model_evaluation import ModelEvaluation
+from src.constants.training_pipeline import SAVED_MODEL_DIR
 
 
 
@@ -84,8 +88,22 @@ class TrainPipeline:
             model_trainer_artifact = model_trainer.initiate_model_trainer()
             return model_trainer_artifact
         except  Exception as e:
-            raise  SensorException(e,sys)
-           
+            raise  MyException(e,sys)
+        
+
+    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact,
+                                ):
+        try:
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+
+            return model_eval_artifact 
+        except  Exception as e:
+            raise  MyException(e,sys)      
         
 
     def run_pipeline(self):
@@ -97,6 +115,11 @@ class TrainPipeline:
              data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
              
              model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+             
+             model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)  
+            
+             if not model_eval_artifact.is_model_accepted:
+                 raise Exception("Trained model is not better than the best model")
              
         except Exception as e :    
             raise  MyException(e,sys)
